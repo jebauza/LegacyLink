ccef291345fa7a577797423dd4e1a0ff0e654c87
Jorge Bauza <jebauza1989@gmail.com>
2021-03-30 12:01:12 +0200

    fix 1

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Api\DeceasedProfileApiController;
 */
 
 
-Route::prefix('public')->middleware(['check_profile'])->group(function () {
+Route::prefix('public')->group(function () {
     Route::get('profile/{profile_id}', [DeceasedProfileApiController::class, 'show'])->name('api.public.profile.show');
     Route::get('profile/{profile_id}/ceremonies', [CeremonyApiController::class, 'indexPublic'])->name('api.public.profile.ceremonies');
     Route::get('profile/{profile_id}/comments/store', [CommentaryApiController::class, 'storePublic'])->name('api.public.profile.ceremonies');
