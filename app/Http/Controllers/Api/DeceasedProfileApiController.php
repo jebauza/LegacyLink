5caeb5a2d421ea2b53b2753dd4adef96e4cb8e20
Jorge Bauza <jebauza1989@gmail.com>
2021-08-30 14:37:18 +0200

    ready add template to profile

--- app/Http/Controllers/Api/DeceasedProfileApiController.php
+++ app/Http/Controllers/Api/DeceasedProfileApiController.php
@@ -114,6 +114,7 @@ class DeceasedProfileApiController extends Controller
             $profile->death = $request->death;
             $profile->title_epitaph = $request->title_epitaph;
             $profile->message_epitaph = $request->message_epitaph;
+            $profile->template = $request->template;
             if($request->photo_base64) {
                 $dirPath = 'deceased_profiles/' . $profile->id;
                 $path = UploadFile::upload($request->photo_base64, $dirPath, true);
